310e86b0e060354a6272c1630dbc73924e89b175
Blackszz <guilherme.pereira411@etec.sp.gov.br>
2024-04-03 10:39:00 -0300

    condicional nadador

---
+++ AulaNivia Condicional/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AulaNivia_Condicional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 Form1 = new Form2();
+            Form1.Show();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtDistancia.Clear();
+            txtPremio.Clear();
+        }
+
+        private void txtDistancia_TextChanged(object sender, EventArgs e)
+        {
+            num1 = 
+        }
+    }
+}
