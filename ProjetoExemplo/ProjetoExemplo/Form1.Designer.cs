6cabb26f4c9bab4c05f5c7feab5cb08c2c2f185c
Blackszz <guilherme.pereira411@etec.sp.gov.br>
2024-05-07 10:47:06 -0300

    loop

---
+++ ProjetoExemplo/ProjetoExemplo/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace ProjetoExemplo
+{
+    partial class frmexemplo
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnumero1 = new System.Windows.Forms.Label();
+            this.lblnumero2 = new System.Windows.Forms.Label();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.txtnumero1 = new System.Windows.Forms.TextBox();
+            this.txtnumero2 = new System.Windows.Forms.TextBox();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.btnlimpar = new System.Windows.Forms.Button();
+            this.btnsair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblnumero1
+            // 
+            this.lblnumero1.AutoSize = true;
+            this.lblnumero1.Location = new System.Drawing.Point(316, 41);
+            this.lblnumero1.Name = "lblnumero1";
+            this.lblnumero1.Size = new System.Drawing.Size(92, 13);
+            this.lblnumero1.TabIndex = 0;
+            this.lblnumero1.Text = "Digite um número:";
+            // 
+            // lblnumero2
+            // 
+            this.lblnumero2.AutoSize = true;
+            this.lblnumero2.Location = new System.Drawing.Point(306, 112);
+            this.lblnumero2.Name = "lblnumero2";
+            this.lblnumero2.Size = new System.Drawing.Size(102, 13);
+            this.lblnumero2.TabIndex = 1;
+            this.lblnumero2.Text = "Digite outro número:";
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(350, 176);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(58, 13);
+            this.lblresultado.TabIndex = 2;
+            this.lblresultado.Text = "Resultado:";
+            // 
+            // txtnumero1
+            // 
+            this.txtnumero1.Location = new System.Drawing.Point(414, 38);
+            this.txtnumero1.Name = "txtnumero1";
+            this.txtnumero1.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero1.TabIndex = 3;
+            // 
+            // txtnumero2
+            // 
+            this.txtnumero2.Location = new System.Drawing.Point(414, 105);
+            this.txtnumero2.Name = "txtnumero2";
+            this.txtnumero2.Size = new System.Drawing.Size(100, 20);
+            this.txtnumero2.TabIndex = 4;
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(414, 169);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.ReadOnly = true;
+            this.txtresultado.Size = new System.Drawing.Size(100, 20);
+            this.txtresultado.TabIndex = 5;
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(376, 227);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 6;
+            this.btncalcular.Text = "Calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // btnlimpar
+            // 
+            this.btnlimpar.Location = new System.Drawing.Point(376, 266);
+            this.btnlimpar.Name = "btnlimpar";
+            this.btnlimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpar.TabIndex = 7;
+            this.btnlimpar.Text = "Limpar";
+            this.btnlimpar.UseVisualStyleBackColor = true;
+            this.btnlimpar.Click += new System.EventHandler(this.btnlimpar_Click);
+            // 
+            // btnsair
+            // 
+            this.btnsair.Location = new System.Drawing.Point(376, 304);
+            this.btnsair.Name = "btnsair";
+            this.btnsair.Size = new System.Drawing.Size(75, 23);
+            this.btnsair.TabIndex = 8;
+            this.btnsair.Text = "Sair";
+            this.btnsair.UseVisualStyleBackColor = true;
+            this.btnsair.Click += new System.EventHandler(this.btnsair_Click);
+            // 
+            // frmexemplo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnsair);
+            this.Controls.Add(this.btnlimpar);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.txtnumero2);
+            this.Controls.Add(this.txtnumero1);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.lblnumero2);
+            this.Controls.Add(this.lblnumero1);
+            this.Name = "frmexemplo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Soma de dois números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnumero1;
+        private System.Windows.Forms.Label lblnumero2;
+        private System.Windows.Forms.Label lblresultado;
+        private System.Windows.Forms.TextBox txtnumero1;
+        private System.Windows.Forms.TextBox txtnumero2;
+        private System.Windows.Forms.TextBox txtresultado;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Button btnlimpar;
+        private System.Windows.Forms.Button btnsair;
+    }
+}
+
